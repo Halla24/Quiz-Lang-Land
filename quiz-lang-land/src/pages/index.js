f9e922967323d54dbc18f676b9297e9ca6767549
halla <aniabisso.16@gmail.com>
2024-02-13 18:15:57 +0100

    change color

--- quiz-lang-land/src/pages/index.js
+++ quiz-lang-land/src/pages/index.js
@@ -8,7 +8,7 @@ const Home = () => {
       <Navbar />
       <div className="bg-cover h-screen flex items-center justify-center" style={{ backgroundImage: 'url("/hohi.avif")' }}>
         <div className="text-center ">
-          <h1 className="text-4xl text-white font-bold">Welcome to the Language Test Levels Platform</h1>
+          <h1 className="text-4xl text-green-500 font-bold">Welcome to the Language Test Levels Platform</h1>
           <p className="text-white mt-8 mb-4">Explore different language test levels and improve your skills.</p>
           <GetStartedButton />
         </div>
