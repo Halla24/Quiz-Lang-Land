e345f2fe887a8900dc3f550c3158379eadc2563d
halla <aniabisso.16@gmail.com>
2024-02-14 13:58:45 +0100

    delete text

--- quiz-lang-land/src/pages/index.js
+++ quiz-lang-land/src/pages/index.js
@@ -9,14 +9,9 @@ const Home = () => {
       <Navbar />
       <AnimatedText />
       <div className="bg-cover h-screen flex items-center justify-center" style={{ backgroundImage: 'url("/hohi.avif")' }}>
-        <div className="text-center">
-          <p className="text-white mt-8 mb-4 animated-fade-in-up-text">
-            Explore different language test levels and improve your skills.
-          </p>
           <GetStartedButton />
         </div>
       </div>
-    </div>
   );
 };
 
