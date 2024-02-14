75b89d3fd43fa76c39cdd4cfab75a5af9741a5b4
halla <aniabisso.16@gmail.com>
2024-02-14 17:40:11 +0100

    add about page

--- quiz-lang-land/src/components/GetStartedButton.jsx
+++ quiz-lang-land/src/components/GetStartedButton.jsx
@@ -4,7 +4,7 @@ const GetStartedButton = () => {
   return (
     <div className='flex items-center justify-center '>
       <div className='button w-40 h-16 bg-blue-500 rounded-lg cursor-pointer select-none
-        active:translate-y-2  active:[box-shadow:0_0px_0_0_#1b6ff8,0_0px_0_0_#1b70f841]
+        active:translate-y-2 active:[box-shadow:0_0px_0_0_#1b6ff8,0_0px_0_0_#1b70f841]
         active:border-b-[0px]
         transition-all duration-150 [box-shadow:0_15px_0_0_#1b6ff8,0_20px_0_0_#1b70f841]
         border-b-[1px] border-blue-400
--- quiz-lang-land/src/pages/about/index.js
+++ quiz-lang-land/src/pages/about/index.js
@@ -1,18 +1,20 @@
 import React from "react";
 import Navbar from "../../components/Navbar";
+import GlassCard from "../../components/GlassCard"; // Import the GlassCard component
 
 const About = () => {
   return (
     <div>
       <Navbar />
       <div
-        className="bg-cover h-screen flex items-center justify-center"
+        className="bg-cover h-screen flex items-center justify-center relative"
         style={{ backgroundImage: 'url("/hohi.avif")' }}
       >
-    </div>
+        {/* GlassCard component with logo.png image and description */}
+        <GlassCard />
+      </div>
     </div>
   );
 };
 
 export default About;
-
