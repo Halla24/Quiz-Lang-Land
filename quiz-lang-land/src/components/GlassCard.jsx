d22bbac2b310d6c8cae2a767f50db45583595eae
halla <aniabisso.16@gmail.com>
2024-02-14 17:49:17 +0100

    fixing about card

--- quiz-lang-land/src/components/GlassCard.jsx
+++ quiz-lang-land/src/components/GlassCard.jsx
@@ -9,11 +9,11 @@ const GlassCard = () => {
     "Embark on a language-learning adventure with our innovative platform! Explore quizzes in various languages, assess your skills, and track your progress seamlessly. Our user-friendly interface makes learning a joy, and our vibrant community ensures you're never alone on your linguistic journey. Start mastering languages with us today!";
 
   return (
-    <div className="bg-opacity-25 bg-gray-900 bg-blur-lg border border-gray-200 rounded-lg overflow-hidden w-100 mx-4 my-0 p-20 flex flex-col items-center text-center">
+    <div className="bg-opacity-25 bg-gray-900 bg-blur-lg border border-gray-200 rounded-lg overflow-hidden w-100 h-80 mx-4 my-0 p-100 flex flex-col items-center text-center">
       <img src={imageUrl} alt={title} className="w-32 h-32 rounded-full mb-4" />
       <div>
         <h2 className="text-xl font-bold text-red-200">{title}</h2>
-        <p className="text-black font-mono ">{description}</p>
+        <p className="text-black font-mono h-50 ">{description}</p>
       </div>
     </div>
   );
